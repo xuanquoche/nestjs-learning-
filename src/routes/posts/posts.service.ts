98bc6e31a02114000d7eef1951e89d86d9366553
xuanquoche <nguyentrinhxuanquoc2003@gmail.com>
2025-12-25 17:23:45 +0700

    config env

--- src/routes/posts/posts.service.ts
+++ src/routes/posts/posts.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@nestjs/common'
+import envConfig from 'src/shared/config'
 import { PrismaService } from 'src/shared/services/prisma.service'
 
 @Injectable()
 export class PostsService {
   constructor(private readonly prismaService: PrismaService) {}
   getPosts() {
+    console.log(envConfig.ACCESS_TOKEN_SECRET)
+
     return this.prismaService.post.findMany()
   }
 
---
+++ src/shared/config.ts
@@ -0,0 +1,48 @@
+import { plainToInstance } from 'class-transformer'
+import { IsNumber, IsString, validateSync } from 'class-validator'
+import { config } from 'dotenv'
+import fs from 'fs'
+import path from 'path'
+
+config({
+  path: '.env',
+})
+
+if (!fs.existsSync(path.resolve('.env'))) {
+  console.log('Please create .env file')
+  process.exit(1)
+}
+
+class ConfigSchema {
+  @IsString()
+  DATABASE_URL: string
+
+  @IsString()
+  ACCESS_TOKEN_SECRET: string
+  @IsString()
+  ACCESS_TOKEN_EXPIRES_IN: string
+  @IsString()
+  REFRESH_TOKEN_SECRET: string
+  @IsString()
+  REFRESH_TOKEN_EXPIRES_IN: string
+}
+
+const configServer = plainToInstance(ConfigSchema, process.env, {
+  enableImplicitConversion: true,
+})
+const e = validateSync(configServer)
+if (e.length > 0) {
+  console.log('cac gia trị không hợp lje')
+  const errors = e.map((error) => {
+    return {
+      property: error.property,
+      value: error.value,
+      constraints: error.constraints,
+    }
+  })
+  throw errors
+}
+
+const envConfig = configServer
+
+export default envConfig
